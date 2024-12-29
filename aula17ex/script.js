029431528d7fbcd330b57b73a7cb48938acb9f7c
Guilherme Matias <guimatias890@gmail.com>
2024-12-29 17:07:56 -0300

    ajuste

--- aula17ex/script.js
+++ aula17ex/script.js
@@ -1,13 +1,28 @@
-function adicionar (){
-let most = document.getElementById('imostranum')
-let n = window.document.getElementById('inum')
 
-if (n.value.length == 0 || Number ( n.value> 100)){
+let n = window.document.getElementById('inum')
+let res = document.getElementById('res')
+let m = document.getElementById('most')
+let valores = []
+
+
+
+function adicionar (){
+if (n.value.length == 0 || Number ( n.value > 100)){
     window.alert ('Número invalido, porfavor preencha corretamente')
-}else {
-    let num = Number(n.value)
+    
+}else{
+    let n1 = Number(n.value)
     let op = document.createElement ('option')
-    op.innerHTML += (`${n}`)
-    most.appendChild (op)
+    op.innerHTML += (`Valor ${n1} foi adicionado.<br>`)
+    res.appendChild (op)
+    valores.push(n1)
+    valores.sort()
+    }
+   
 }
+
+function finalizar (){
+      m.innerHTML +=`Ao todo temos ${valores.length} números cadastrado. <br>`
+      m.innerHTML +=`O maior número é o ${valores}`
+        
 }
