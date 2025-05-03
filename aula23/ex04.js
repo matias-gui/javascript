6b89098539e870f3ab7930e2e200b6593ce326ed
Guilherme Matias <guimatias890@gmail.com>
2025-05-03 11:26:29 -0300

    ajuste no exercício de função

--- aula23/ex04.js
+++ aula23/ex04.js
@@ -1,15 +1,22 @@
+// Criação de uma tabela do IMC, que irá imprimir um mensagem sobre o seu peso.
+function Imc(peso, altura) {
+    let calcImc = peso / (altura * altura)
+    let mensagem = ""
 
-function Imc(peso,altura){
- let calcImc = peso / (altura * altura)
-
-if(calcImc < 18.5){
-    console.log( "Baixo peso")
-}else if(calcImc >= 18.5 && calcImc <= 24.9){
-    console.log("Peso normal")
-}else if(calcImc >= 25 && calcImc <= 29.9){
-    console.log("Sobre Peso")
+    if (calcImc < 18.5) {
+        mensagem = "De acordo com a tabela de IMC, você está com baixo peso"
+    } else if (calcImc >= 18.5 && calcImc <= 24.9) {
+        mensagem = " De acordo com a tabela de IMC, você está no seu peso normal"
+    } else if (calcImc >= 25 && calcImc <= 29.9) {
+        mensagem = "De acordo com a tabela de IMC, você está acima do peso"
+    } else if (calcImc >= 30 && calcImc <= 34.99) {
+        mensagem = "De acordo com a tabela de IMC, você está com obesidade 1"
+    } else if (calcImc >= 35 && calcImc <= 39.99) {
+        mensagem = "De acordo com a tabela de IMC, você está com obesidade 2 (severa)"
+    } else {
+        mensagem = "De acordo com a tabela de IMC, você está com obesidade 3 (mórbida)"
+    }
+    return mensagem
 }
-}
 
-console.log(Imc(83, 175
-))
+console.log(Imc(83, 1.75))
