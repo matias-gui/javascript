75019b57e763826c4d7860cc4df053b40801cbd7
Guilherme Matias <guimatias890@gmail.com>
2025-05-12 22:29:11 -0300

    classe

--- aula27/ex03.js
+++ aula27/ex03.js
@@ -1,7 +1,23 @@
 
 class Estudante {
-    constructor(notas){
-        this.nota = [notas]
+    constructor(){
+        this.notas = []
     }
+    adicionarNota(notaProva){
+        this.notas.push(notaProva)
+    }
+    calcularMedia(){
+      let soma = 0 
+      for(let i in this.notas){
+        soma += this.notas[i]
+      }
+      let media = soma / this.notas.length
+      console.log(media)
 
+    }
 }
+
+let media = new Estudante()
+media.adicionarNota(5)
+media.adicionarNota(1)
+media.calcularMedia()
