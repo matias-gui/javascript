3b53992820e58929dbf437d41a0c7f77e75844b9
Guilherme Matias <guimatias890@gmail.com>
2025-05-20 20:11:08 -0300

    desestruturação

---
+++ aula30/ex01.js
@@ -0,0 +1,8 @@
+// Extraindo Dados de um Array usando a desestruturação.
+
+let array = ["Maça", "Banana", "Pera"]
+
+//Agora Maça pertence a vermelho, Banana a amarelo e Pera a verde
+let [vermelho, amarelo, verde] = array
+
+console.log(vermelho, amarelo, verde)
---
+++ aula30/ex02.js
@@ -0,0 +1,7 @@
+// Extraindo propriedade de um objeto, usando a desestruturação.
+
+ let pessoa = {nome: "Guilherme", idade: 22}
+
+ let {nome, idade} = pessoa
+
+ console.log("Nome:",nome,"Idade:",idade)
